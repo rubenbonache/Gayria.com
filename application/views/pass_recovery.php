1c25cc3c5a208ea69e58944fa1cf090ed61b6971
rubenbonache <ruben.bonache@gmail.com>
2012-08-22 12:09:36 +0100

    Template creado

--- application/views/pass_recovery.php
+++ application/views/pass_recovery.php
@@ -1,4 +1,10 @@
 <div class="table">
   
 <p class="titulosficha pfinder">Recuperar contraseña</p>
+
+<p class="titulosvideo pfinder">Para recuperar tu contraseña es necesario que especifiques el correo electronico con el que se registro.</p>
+
+<form>
+	<table width="650px"><tr><td class="titulosvideo" valign="middle">Introduce el correo electronico</td><td valign="middle"><input type="text" name="email" class="field2" size="50"> <input type="submit" name="Enviar" class="button-1"></td></tr></table>
+</form>
 </div>
